8b7c1a16c2ecf3790f2d32543e8bbf3d3d30c250
Oleksii Druzhynin <Oleksii_Druzhynin@epam.com>
2020-04-19 16:35:07 -0700

    Clean-up.

--- django/.env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\projects\computer_vision\competency\repos\model_garden\.env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
