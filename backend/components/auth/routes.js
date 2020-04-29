ec10a46e83a70342fe42deae6a3e67469ff79e88
Aliaksandr Shautsou <aliaksandr_shautsou@epam.com>
2020-04-29 10:03:44 +0300

    added error handling in the backend, fixes on frontend and backend

--- backend/components/auth/routes.js
+++ backend/components/auth/routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const authController = require('./controllers');
 const router = express.Router();
 
-router.use('/login', authController.login);
+router.use('/login_cvat', authController.loginCvat);
 
 module.exports = router;
