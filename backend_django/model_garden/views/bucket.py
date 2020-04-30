a729b70b84a9a1607a9d7e2d39256c2ca40dec50
skudriashev <stas.kudriashev@gmail.com>
2020-04-29 20:44:19 -0700

    Add bucket model and initial migration

---
+++ backend_django/model_garden/views/bucket.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from model_garden.models.bucket import Bucket
+from model_garden.serializers import BucketSerializer
+
+
+class BucketViewSet(viewsets.ModelViewSet):
+    queryset = Bucket.objects.all()
+    serializer_class = BucketSerializer
