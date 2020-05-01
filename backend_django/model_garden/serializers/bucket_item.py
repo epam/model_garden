7286642c805a8a336fc0f01b2171303b95a74a42
skudriashev <stas.kudriashev@gmail.com>
2020-05-01 16:09:40 -0700

    Implement media assets upload API

--- backend_django/model_garden/serializers/bucket_item.py
+++ backend_django/model_garden/serializers/bucket_item.py
@@ -1,12 +1,36 @@
+from datetime import datetime
+
 from rest_framework import serializers
 
 from model_garden.models import BucketItem
 
 
 class BucketItemSerializer(serializers.ModelSerializer):
+  path = serializers.CharField(allow_blank=True, allow_null=True)
+
   class Meta:
     model = BucketItem
     fields = (
       'path',
       'bucket',
     )
+
+  def validate(self, attrs):
+    path = attrs.get('path')
+    if path is None:
+      path = ''
+
+    path = path.strip()
+    if not path:
+      attrs['path'] = f'batch_{datetime.utcnow().date()}'
+    return super().validate(attrs=attrs)
+
+  def create(self, validated_data):
+    bucket_item = BucketItem.objects.filter(
+      path=validated_data['path'],
+      bucket=validated_data['bucket'],
+    ).first()
+    if bucket_item is not None:
+      return bucket_item
+
+    return super().create(validated_data=validated_data)
