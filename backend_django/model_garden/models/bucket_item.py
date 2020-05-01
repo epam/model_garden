1b24c870f9c707356c7f31ddc5a752ad776dd245
skudriashev <stas.kudriashev@gmail.com>
2020-05-01 09:15:09 -0700

    Add bucket item model

---
+++ backend_django/model_garden/models/bucket_item.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from model_garden.models import Bucket
+
+
+class BucketItem(models.Model):
+    path = models.CharField(max_length=1024)
+    bucket = models.ForeignKey(Bucket, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(path='{self.path}', bucket={self.bucket})"
