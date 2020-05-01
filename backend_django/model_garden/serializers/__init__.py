f7c862f594ef5f530c224415743e7b3b96dc952f
skudriashev <stas.kudriashev@gmail.com>
2020-05-01 11:26:53 -0700

    Add media asset serializer

--- backend_django/model_garden/serializers/__init__.py
+++ backend_django/model_garden/serializers/__init__.py
@@ -1,5 +1,7 @@
 from .bucket import BucketSerializer
+from .media_asset import MediaAssetSerializer
 
 __all__ = (
   "BucketSerializer",
+  "MediaAssetSerializer",
 )
--- backend_django/model_garden/views/__init__.py
+++ backend_django/model_garden/views/__init__.py
@@ -1,5 +1,7 @@
 from .bucket import BucketViewSet
+from .media_asset import MediaAssetViewSet
 
 __all__ = (
   "BucketViewSet",
+  "MediaAssetViewSet",
 )
