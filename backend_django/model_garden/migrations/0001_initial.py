a86e8175b77b3bda21a9d40cd614eb4248dc18a8
skudriashev <stas.kudriashev@gmail.com>
2020-05-01 09:24:57 -0700

    Use 2 space indent

--- backend_django/model_garden/migrations/0001_initial.py
+++ backend_django/model_garden/migrations/0001_initial.py
@@ -4,18 +4,17 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
+  initial = True
 
-    initial = True
+  dependencies = [
+  ]
 
-    dependencies = [
-    ]
-
-    operations = [
-        migrations.CreateModel(
-            name='Bucket',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=63)),
-            ],
-        ),
-    ]
+  operations = [
+    migrations.CreateModel(
+      name='Bucket',
+      fields=[
+        ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+        ('name', models.CharField(max_length=63)),
+      ],
+    ),
+  ]
