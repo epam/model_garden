969ef74b372852d2d8ffd803ac7e8139dad6fc6b
skudriashev <stas.kudriashev@gmail.com>
2020-05-01 09:24:57 -0700

    Use 2 space indent

--- backend_django/model_garden/migrations/0002_bucketitem.py
+++ backend_django/model_garden/migrations/0002_bucketitem.py
@@ -5,18 +5,17 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
+  dependencies = [
+    ('model_garden', '0001_initial'),
+  ]
 
-    dependencies = [
-        ('model_garden', '0001_initial'),
-    ]
-
-    operations = [
-        migrations.CreateModel(
-            name='BucketItem',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('path', models.CharField(max_length=1024)),
-                ('bucket', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='model_garden.Bucket')),
-            ],
-        ),
-    ]
+  operations = [
+    migrations.CreateModel(
+      name='BucketItem',
+      fields=[
+        ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+        ('path', models.CharField(max_length=1024)),
+        ('bucket', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='model_garden.Bucket')),
+      ],
+    ),
+  ]
