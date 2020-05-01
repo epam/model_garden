cf5bc03d313e2a312e4f262b0fdbe5dafbf5b353
skudriashev <stas.kudriashev@gmail.com>
2020-05-01 16:09:40 -0700

    Implement media assets upload API

--- backend_django/model_garden/models/bucket_item.py
+++ backend_django/model_garden/models/bucket_item.py
@@ -7,5 +7,8 @@ class BucketItem(models.Model):
   path = models.CharField(max_length=512)
   bucket = models.ForeignKey(Bucket, on_delete=models.CASCADE)
 
+  class Meta:
+    unique_together = [['path', 'bucket']]
+
   def __str__(self):
-    return f"{self.__class__.__name__}(path='{self.path}', bucket={self.bucket})"
+    return f"{self.__class__.__name__}(path='{self.path}', bucket='{self.bucket.name}')"
