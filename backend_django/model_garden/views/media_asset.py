c3c610f59542828f8d70bdb93485d54583d42ba7
skudriashev <stas.kudriashev@gmail.com>
2020-05-03 14:09:35 -0700

    Implement zip media asset upload

--- backend_django/model_garden/views/media_asset.py
+++ backend_django/model_garden/views/media_asset.py
@@ -1,3 +1,7 @@
+from concurrent.futures import ThreadPoolExecutor
+import io
+import zipfile
+
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -50,9 +54,33 @@ class MediaAssetViewSet(viewsets.ModelViewSet):
     dataset = dataset_serializer.save()
 
     s3_client = S3Client(bucket_name=bucket.name)
+    files_to_upload = []
     for file in files:
-      media_asset = MediaAsset(dataset=dataset, filename=file.name)
-      s3_client.upload_file_obj(file_obj=file.file, bucket=bucket.name, key=media_asset.full_path)
-      media_asset.save()
+      if file.content_type == 'application/zip':
+        if not zipfile.is_zipfile(file.file):
+          return Response(
+            data={
+              "message": f"File '{file.name}' is not a zip file",
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+          )
+
+        zip_file = zipfile.ZipFile(file.file)
+        for zip_filename in zip_file.filelist:
+          with zip_file.open(zip_filename) as fp:
+            zip_file_obj = io.BytesIO(fp.read())
+            files_to_upload.append((zip_filename.filename, zip_file_obj))
+      elif 'image' in file.content_type:
+        files_to_upload.append((file.name, file.file))
+      else:
+        continue
+
+    with ThreadPoolExecutor(max_workers=8) as executor:
+      for file_name, file_obj in files_to_upload:
+        executor.submit(self._upload_file_to_s3, s3_client, dataset, file_name, file_obj)
 
     return Response()
+
+  def _upload_file_to_s3(self, s3_client, dataset, file_name, file_obj):
+    media_asset = MediaAsset.objects.create(dataset=dataset, filename=file_name)
+    s3_client.upload_file_obj(file_obj=file_obj, bucket=dataset.bucket.name, key=media_asset.full_path)
