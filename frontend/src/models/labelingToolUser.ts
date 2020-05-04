d123c6e5772b85844d89e8dd407cd9c7793f0759
skudriashev <stas.kudriashev@gmail.com>
2020-05-04 15:43:57 -0700

    Implement auth to CVAT and getting users

--- frontend/src/models/labelingToolUser.ts
+++ frontend/src/models/labelingToolUser.ts
@@ -1,3 +1,3 @@
 export class LabelingToolUser {
-  constructor(public name: string, public id: string, public email: string) {}
+  constructor(public full_name: string, public id: string, public email: string) {}
 }
