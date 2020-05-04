4a8fdcd14602351313459653013af0b3d09e006d
skudriashev <stas.kudriashev@gmail.com>
2020-05-04 15:43:57 -0700

    Implement auth to CVAT and getting users

--- backend_django/model_garden/serializers/__init__.py
+++ backend_django/model_garden/serializers/__init__.py
@@ -1,9 +1,11 @@
 from .bucket import BucketSerializer
+from .cvat_user import CvatUserSerializer
 from .dataset import DatasetSerializer
 from .media_asset import MediaAssetSerializer
 
 __all__ = (
   "BucketSerializer",
+  "CvatUserSerializer",
   "DatasetSerializer",
   "MediaAssetSerializer",
 )
