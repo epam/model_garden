0217eba86d32252e69e3270054ddf18e853c087a
skudriashev <stas.kudriashev@gmail.com>
2020-05-07 09:47:35 -0700

    Remove legacy (nodejs) backend

--- backend/components/auth/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const authController = require('./controllers');
-const router = express.Router();
-
-router.use('/login_cvat', authController.loginCvat);
-
-module.exports = router;
