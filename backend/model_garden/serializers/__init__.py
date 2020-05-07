06c4d31400180f7978ed477a1d8c7e1c111d1055
Artem Mustafa <artem_mustafa@epam.com>
2020-05-07 12:02:48 -0700

    Merged post/get tasks. Added call to cvat-tasks in fontend

--- backend/model_garden/serializers/__init__.py
+++ backend/model_garden/serializers/__init__.py
@@ -2,13 +2,13 @@ from .bucket import BucketSerializer
 from .cvat_user import CvatUserSerializer
 from .dataset import DatasetSerializer
 from .media_asset import MediaAssetSerializer
-from .task import TaskSerializer, CvatTaskSerializer
+from .task import CvatTaskCreateSerializer, CvatTaskSerializer
 
 __all__ = (
   "BucketSerializer",
-  "CvatTaskSerializer"
+  "CvatTaskCreateSerializer",
+  "CvatTaskSerializer",
   "CvatUserSerializer",
   "DatasetSerializer",
   "MediaAssetSerializer",
-  "TaskSerializer",
 )
--- backend/model_garden/views/cvat_tasks.py
+++ backend/model_garden/views/cvat_tasks.py
@@ -8,14 +8,32 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.exceptions import ValidationError
 
-from model_garden.serializers import CvatTaskSerializer
+from model_garden.serializers import (
+  CvatTaskSerializer, CvatTaskCreateSerializer
+)
 from model_garden.services.cvat import (
-  CvatService, ListRequest
+  CvatService, CVATServiceException, ListRequest
 )
 
 
 class CvatTaskViewSet(ViewSet):
-  serializer_class = CvatTaskSerializer
+  serializer_class = CvatTaskCreateSerializer
+
+  def create(self, request):
+    cvat_service = CvatService()
+    serializer = self.serializer_class(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    data = serializer.data
+    try:
+      cvat_service.create_task(
+        name=data['task_name'],
+        assignee_id=data['assignee_id'],
+        owner_id=cvat_service.get_root_user()['id'],
+      )
+    except CVATServiceException as e:
+      return Response(data={'message': str(e)}, status=400)
+
+    return Response()
 
   def list(self, request: Request):
     queryset = CvatTasksQuerySet(CvatService())
