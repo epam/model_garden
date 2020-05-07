9f296691f703a9ebf165b00179d8c87236833f79
skudriashev <stas.kudriashev@gmail.com>
2020-05-07 14:50:29 -0700

    Implement docker image build for frontend

--- backend/model_garden/settings.py
+++ backend/model_garden/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_WHITELIST = [
   # frontend
+  "http://localhost",
+  "http://127.0.0.1",
   "http://localhost:4200",
   "http://127.0.0.1:4200",
 ]
--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,3 +1,4 @@
+export * from "./const"
 export * from "./labelingTask.api";
-export * from "./media.api";
 export * from "./main.api";
+export * from "./media.api";
