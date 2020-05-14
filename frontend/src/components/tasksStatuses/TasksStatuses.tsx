c09cd66f054d838bfb336970c0aac92b3834d52a
Sergei Iakimenko <sergei_iakimenko@epam.com>
2020-05-13 20:53:47 -0400

    Adds loading tasks on loading

--- frontend/src/components/tasksStatuses/TasksStatuses.tsx
+++ frontend/src/components/tasksStatuses/TasksStatuses.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MUIDataTable from "mui-datatables";
 import './TasksStatuses.css';
 import { TABLE_TITLE, TASK_STATUSES_COLUMNS, TASK_STATUSES } from './constants';
+import {useDispatch, useSelector} from "react-redux";
+import {AppState} from "../../store";
+import {getDatasets, getLabelingTasks} from "../../store/labelingTask";
 
 const options = {
   filterType: 'textField' as "checkbox" | "dropdown" | "multiselect" | "textField" | "custom" | undefined,
@@ -11,11 +14,33 @@ const options = {
 };
 
 export const TasksStatuses: React.FC = () => {
+  const dispatch = useDispatch();
+  const buckets = useSelector((state: AppState) => state.main.buckets);
+  const currentBucketId = useSelector((state: AppState) => state.labelingTask.currentBucketId);
+  const datasets = useSelector((state: AppState) => state.labelingTask.datasets);
+  const currentDatasetId = useSelector((state: AppState) => state.labelingTask.currentDatasetId);
+
+  const tasks = useSelector(
+    (state: AppState) => state.labelingTask.labelingTasksStatuses
+  );
+
+  useEffect(() => {
+    if (currentBucketId) {
+      dispatch(getDatasets(currentBucketId));
+      getTasks();
+    }
+  }, [dispatch, currentBucketId]);
+
+  const getTasks = () => {
+    dispatch(getLabelingTasks(currentBucketId, currentDatasetId));
+  };
+
+
   return (
       <div className={'task-statuses'}>
           <MUIDataTable
               title={TABLE_TITLE}
-              data={TASK_STATUSES}
+              data={tasks}
               columns={TASK_STATUSES_COLUMNS}
               options={options}
           />
