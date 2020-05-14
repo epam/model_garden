55d7be63dee1eb9c5cc85b52b701e52e076f9c0e
Sergei Iakimenko <sergei_iakimenko@epam.com>
2020-05-14 23:58:10 +0000

    Issue/#10

--- frontend/src/components/tasksStatuses/TasksStatuses.tsx
+++ frontend/src/components/tasksStatuses/TasksStatuses.tsx
@@ -1,39 +1,45 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
+import MUIDataTable from "mui-datatables";
 import './TasksStatuses.css';
-import { ImagesLocation } from '../shared/imagesLocation';
-import { AppState } from '../../store';
-import { useDispatch, useSelector } from 'react-redux';
-import { Tasks } from './tasks';
-import { getLabelingTasks, getDatasets } from '../../store/labelingTask';
+import { TABLE_TITLE, TASK_STATUSES_COLUMNS } from './constants';
+import {useDispatch, useSelector} from "react-redux";
+import {AppState} from "../../store";
+import {getDatasets, getLabelingTasks} from "../../store/labelingTask";
+
+const options = {
+  filterType: 'textField' as "checkbox" | "dropdown" | "multiselect" | "textField" | "custom" | undefined,
+  download: false,
+  print: false,
+  selectableRows: 'none' as "none" | "multiple" | "single" | undefined
+};
 
 export const TasksStatuses: React.FC = () => {
   const dispatch = useDispatch();
-  const buckets = useSelector((state: AppState) => state.main.buckets);
   const currentBucketId = useSelector((state: AppState) => state.labelingTask.currentBucketId);
-  const datasets = useSelector((state: AppState) => state.labelingTask.datasets);
   const currentDatasetId = useSelector((state: AppState) => state.labelingTask.currentDatasetId);
+
+  const tasks = useSelector(
+    (state: AppState) => state.labelingTask.labelingTasksStatuses
+  );
 
   useEffect(() => {
     if (currentBucketId) {
       dispatch(getDatasets(currentBucketId));
     }
   }, [dispatch, currentBucketId]);
-  
-  const handleImagesLocationSubmit = () => {
+
+  useEffect(() => {
     dispatch(getLabelingTasks(currentBucketId, currentDatasetId));
-  };
+  }, []);
 
   return (
-    <div className="tasks-statuses">
-      <ImagesLocation
-        title="Select images location"
-        buttonName="Get tasks info"
-        buckets={buckets}
-        datasets={Array.from(datasets.values())}
-        currentBucketId={currentBucketId}
-        handleFormSubmit={handleImagesLocationSubmit}
-      />
-      <Tasks />
-    </div>
+      <div className={'task-statuses'}>
+          <MUIDataTable
+              title={TABLE_TITLE}
+              data={tasks}
+              columns={TASK_STATUSES_COLUMNS}
+              options={options}
+          />
+      </div>
   );
 };
