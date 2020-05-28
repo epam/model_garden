2b750d5175df285d4bda2aac123096b0d52ffd44
skudriashev <stas.kudriashev@gmail.com>
2020-05-28 11:36:12 -0700

    Add remote image/xml links to media assets API response

--- backend/model_garden/serializers/media_asset.py
+++ backend/model_garden/serializers/media_asset.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 
+from model_garden.constants import LabelingTaskStatus
 from model_garden.models import MediaAsset
 
 
 class MediaAssetSerializer(serializers.ModelSerializer):
+  remote_xml_path = serializers.SerializerMethodField()
+
   class Meta:
     model = MediaAsset
     fields = (
       'dataset',
       'filename',
+      'remote_path',
+      'remote_xml_path',
     )
+
+  def get_remote_xml_path(self, obj: MediaAsset) -> str:
+    if obj.labeling_task and obj.labeling_task.status == LabelingTaskStatus.SAVED:
+      return obj.remote_xml_path
