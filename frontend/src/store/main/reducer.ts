6f6e3a94390003153d30cb59ce71201cfc03e9f0
Luis Suarez <Luis_Suarez@epam.com>
2020-05-29 04:40:53 -0500

    Fix ErrorAlert for status 500 and add Router
    - Move main layout to app.tsx
    - Change nullish operator to default param on ErrorAlert
    - Add Root router to persist page location across reloads
    - Update ErrorAlert to handle it's own state
    - remove  setSelectedMenuItem from state

--- frontend/src/store/main/reducer.ts
+++ frontend/src/store/main/reducer.ts
@@ -1,19 +1,16 @@
 import {
   MainActionTypes,
-  SET_SELECTED_MENU_ITEM,
   GET_BUCKETS_START,
   GET_BUCKETS_SUCCESS,
 } from "./types";
 import {Bucket} from "../../models";
 
 export interface MainState {
-  selectedMenuItemIndex: number;
   isBucketsLoading: boolean;
   buckets: Bucket[];
 }
 
 const initialState: MainState = {
-  selectedMenuItemIndex: 0,
   isBucketsLoading: false,
   buckets: [],
 };
@@ -23,11 +20,6 @@ export const mainReducer = (
   action: MainActionTypes
 ): MainState => {
   switch (action.type) {
-    case SET_SELECTED_MENU_ITEM:
-      return {
-        ...state,
-        selectedMenuItemIndex: action.menuItemIndex
-      }
     case GET_BUCKETS_START:
       return {
         ...state,
