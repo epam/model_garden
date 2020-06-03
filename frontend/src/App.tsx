1c823cb560783054faf8ccc83e6bdf618bd19dff
Svitlana Tsar <svitlana_tsar@epam.com>
2020-06-03 01:30:40 +0000

    Issue 55: Add loading bar / spinner to all submit forms

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, FC } from 'react';
 import { useDispatch } from 'react-redux';
 import { AppBar } from '@material-ui/core';
+import { ThemeProvider } from "@material-ui/core/styles";
 import { useLocation, Redirect } from 'react-router-dom';
 import { ErrorAlert } from './components';
 import { getBuckets } from './store/main';
 import { getLabelingToolUsers } from './store/labelingTask'
 import { Tabs, LinkTabs } from './routerconfig';
+import theme from "./theme";
 
 import './App.css';
 
@@ -23,13 +25,13 @@ const App: FC = () => {
   }
 
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <ErrorAlert />
       <AppBar position="static">
         <LinkTabs pathname={pathname} />
       </AppBar>
       <Tabs />
-    </div>
+    </ThemeProvider>
   );
 };
 
