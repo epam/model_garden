89c0e5936c6672ea547cf570f92b16a01a69dbf4
Luis Suarez <Luis_Suarez@epam.com>
2020-06-04 17:33:04 -0500

    issue 59 : add filter to Gallery

--- frontend/src/components/gallery/Gallery.tsx
+++ frontend/src/components/gallery/Gallery.tsx
@@ -1,61 +1,137 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { AppState } from '../../store';
+import { Bucket, Dataset } from '../../models';
+import { Grid, Container, Select, FormControl, InputLabel, MenuItem } from '@material-ui/core';
+import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import {useCurrentWitdh} from './useWidth';
+import { getMediaImages } from '../../store/media';
+import { getDatasets } from '../../store/labelingTask';
+import { GridGallery } from './GridGallery';
+
+import { setCurrentBucketId, setCurrentDatasetId } from '../../store/labelingTask';
 
 import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-    padding: 30,
-    paddingTop: 20
-  },
-  gridList: {
-    width: '100%',
-    height: '100%'
+  item: {
+    [theme.breakpoints.down('sm')]: {
+      width: `${100 / 3}%`
+    },
+    [theme.breakpoints.only('md')]: {
+      width: `${100 / 4}%`
+    },
+    [theme.breakpoints.only('lg')]: {
+      width: `${100 / 6}%`
+    },
+    [theme.breakpoints.up('xl')]: {
+      width: `${100 / 10}% `
+    }
   }
 }));
 
-const Gallery = () => {
-  const [fotos, setFotos] = useState([]);
-  const width = useCurrentWitdh();
-  console.log(width)
-  let numberOfColumns;
+interface GalleryProps {
+  photos: [{}];
+  buckets: any;
+  currentBucketId: any;
+  dataset: any;
+}
 
-  if (width > 2000) {
-    numberOfColumns = 8;
-  }else if (width > 1700) {
-    numberOfColumns = 7;
-  } else if (width > 1200) {
-    numberOfColumns = 5;
-  } else if (width > 720) {
-    numberOfColumns = 4;
-  } else if (width > 500) {
-    numberOfColumns = 3;
-  }
-
+const GalleryComponent = (props: any) => {
+  const {
+    photos,
+    buckets,
+    currentBucketId,
+    datasets,
+    getMediaImages,
+    setCurrentBucketId,
+    currentDatasetId,
+    setCurrentDatasetId,
+    getDatasets
+  } = props;
   const classes = useStyles();
+
   useEffect(() => {
-    axios.get('http://localhost:9000/api/media-assets/').then(({ data }: any) => setFotos(data.results));
-  }, []);
+    if (currentDatasetId) {
+      getMediaImages({ bucketId: currentBucketId, datasetId: currentDatasetId });
+    }
+  }, [currentDatasetId]);
+
+  useEffect(() => {
+    if (currentBucketId) {
+      getDatasets(currentBucketId);
+    }
+  }, [currentBucketId]);
+
+  const handleBucketChange = (e: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+    if (e.target.value) {
+      setCurrentBucketId(e.target.value as string);
+    }
+  };
+  const handleDataSetChange = (e: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+    if (e.target.value) {
+      setCurrentDatasetId(e.target.value as string);
+    }
+  };
+
 
   return (
-    <div className={classes.root}>
-      <GridList cellHeight={160} className={classes.gridList} cols={numberOfColumns}>
-        {fotos.map((tile: any) => (
-          <GridListTile key={tile.filename+tile.remote_path+tile.dataset} cols={1}>
-            <img src={tile.remote_path} alt={'picture'} />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
+    <Container maxWidth={'xl'}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={3}>
+          <FormControl className="task__form-item">
+            <InputLabel id="task-bucket-name">Bucket</InputLabel>
+            <Select
+              labelId="task-bucket-name"
+              name="bucketId"
+              variant="outlined"
+              label="Bucket"
+              value={currentBucketId}
+              onChange={handleBucketChange}
+            >
+              {buckets.map((bucket: Bucket, index: number) => (
+                <MenuItem key={index} value={bucket.id}>
+                  {bucket.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <FormControl className="task__form-item">
+            <InputLabel id="task-bucket-name">Dataset</InputLabel>
+            <Select
+              labelId="task-bucket-name"
+              name="selectedDataset"
+              variant="outlined"
+              label="Dataset"
+              value={currentDatasetId}
+              onChange={handleDataSetChange}
+            >
+              {datasets.map((dataset: Dataset, index: any) => (
+                <MenuItem key={index} value={dataset.id}>
+                  {dataset.path}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+
+      <GridGallery photos={photos} />
+    </Container>
   );
 };
-
-export { Gallery };
+const mstp = ({ media, main, labelingTask }: AppState) => ({
+  photos: media.photos,
+  buckets: main.buckets,
+  currentBucketId: labelingTask.currentBucketId,
+  currentDatasetId: labelingTask.currentDatasetId,
+  datasets: Array.from(labelingTask.datasets.values())
+});
+const actions = {
+  setCurrentBucketId,
+  setCurrentDatasetId,
+  getMediaImages,
+  getDatasets
+};
+export const Gallery = connect(mstp, actions)(GalleryComponent);
