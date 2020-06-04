2bcca9169ba44472b175077e07577cb59952ed84
Svitlana Tsar <svitlana_tsar@epam.com>
2020-06-04 13:10:56 -0400

    Issue 43: Implement task statuses table ordering

--- frontend/src/store/labelingTask/actions.ts
+++ frontend/src/store/labelingTask/actions.ts
@@ -170,10 +170,12 @@ export const createLabelingTask = (
 export const getLabelingTasks = (
   page: number,
   rowsPerPage: number,
-  filterMap: any
+  filterMap: any,
+  sortOrder?: string | undefined,
+  sortField?: string | undefined
 ): AppThunk => (dispatch) => {
   dispatch(getLabelingTasksStart());
-  return getLabelingTasksRequest(page, rowsPerPage, filterMap)
+  return getLabelingTasksRequest(page, rowsPerPage, filterMap, sortOrder, sortField)
     .then((tasksData) => dispatch(getLabelingTasksSuccess(tasksData)))
     .catch((error) => dispatch(setErrorAction(error)));
 };
