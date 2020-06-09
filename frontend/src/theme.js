c7ba335fa49712fbe8807877061fbc168ffd6739
Svitlana Tsar <svitlana_tsar@epam.com>
2020-06-09 17:25:40 -0400

    Add title styles to the theme

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,6 +1,21 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
+  typography: {
+    h1: {
+      textAlign: 'center',
+      fontSize: '1.5rem',
+      fontWeight: '400'
+    },
+    h2: {
+      fontSize: '1.35rem',
+      fontWeight: '400'
+    },
+    h3: {
+      fontSize: '1.25rem',
+      fontWeight: '400'
+    }
+  },
   props: {
     MuiGridList: {
       cols: null,
