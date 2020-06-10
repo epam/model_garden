62f8971c60f51767d4a5d797620dc282160fbc4c
Svitlana Tsar <svitlana_tsar@epam.com>
2020-06-10 13:58:36 -0400

    Add background color

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,6 +1,9 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
+  background: {
+    default: '#ffffff'
+  },
   typography: {
     h1: {
       textAlign: 'center',
