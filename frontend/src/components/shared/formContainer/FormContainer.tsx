9d4c8cb4b961d0aeb2197efe1e670b660e5675a0
Svitlana Tsar <svitlana_tsar@epam.com>
2020-06-10 12:44:26 -0400

    Improve padding. For left and right use default

--- frontend/src/components/shared/formContainer/FormContainer.tsx
+++ frontend/src/components/shared/formContainer/FormContainer.tsx
@@ -4,7 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
-    padding: '2.5rem'
+    paddingTop: '2.5rem',
+    paddingBottom: '2.5rem'
   }
 });
 
