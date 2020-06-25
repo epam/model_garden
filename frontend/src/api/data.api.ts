a538155af9d2ce8860699d278ff5d14785949849
Svitlana Tsar <svitlana_tsar@epam.com>
2020-06-25 14:40:55 -0400

    Create Data Store

---
+++ frontend/src/api/data.api.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { backendHostPort } from './environment';
+
+export const getBucketsRequest = async () => {
+  try {
+    return await axios.get(`http://${backendHostPort}/api/buckets/`);
+  } catch (error) {
+    if (error && error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw error;
+    }
+  }
+};
+
+export const getDatasetsRequest = async (bucketId: string) => {
+  try {
+    return await axios.get(`http://${backendHostPort}/api/datasets/`, {
+      params: {
+        bucket_id: bucketId
+      }
+    });
+  } catch (error) {
+    if (error && error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw error;
+    }
+  }
+};
+
+export const getMediaAssetsRequest = ({ bucketId, datasetId }: any) => {
+  const url = `http://${backendHostPort}/api/media-assets/${
+    bucketId
+      ? `?bucket_Id=${bucketId}${datasetId ? `&dataset_id=${datasetId}` : ''}`
+      : datasetId
+      ? `?dataset_id=${datasetId}`
+      : ''
+  }`;
+  return axios.get(url);
+};
