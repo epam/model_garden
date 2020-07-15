56d8bf9b930c3bda978461b256791397c38a6b7c
Sergii Borsuk <sergii_borsuk@epam.com>
2020-07-14 20:01:40 -0500

    Return xml path for saved and archived tasks.

--- backend/model_garden/serializers/media_asset.py
+++ backend/model_garden/serializers/media_asset.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from model_garden.constants import LabelingTaskStatus
 from model_garden.models import MediaAsset
 
 
@@ -16,5 +17,6 @@ class MediaAssetSerializer(serializers.ModelSerializer):
     )
 
   def get_remote_xml_path(self, obj: MediaAsset) -> str:
-    if obj.labeling_task:
-      return obj.remote_xml_path
+      if obj.labeling_task and (obj.labeling_task.status == LabelingTaskStatus.SAVED
+                                or obj.labeling_task.status == LabelingTaskStatus.ARCHIVED):
+          return obj.remote_xml_path
