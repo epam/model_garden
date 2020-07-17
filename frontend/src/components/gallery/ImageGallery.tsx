425c966e413da34a72c96e509af30465b2324f91
Elaine Onuegbe <elaine_onuegbe@epam.com>
2020-07-17 09:56:34 -0700

    added search input to imageGallery

--- frontend/src/components/gallery/ImageGallery.tsx
+++ frontend/src/components/gallery/ImageGallery.tsx
@@ -1,11 +1,21 @@
-import React, { useEffect } from 'react';
-import { Container, Grid } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import {
+  Container,
+  Grid,
+  Select,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  TextField,
+  InputAdornment
+} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import { useRouteMatch, Redirect, Link } from 'react-router-dom';
 import { Empty } from 'antd';
 
 import { useTypedSelector, useAppDispatch } from '../../store';
 import { getMediaAssets } from '../../store/data';
-import { ImageCard } from './ImageCard';
+import { ImageCard } from './imageCard';
 import { ImageGalleryHeader } from './ImageGalleryHeader';
 import { Dataset } from '../../models';
 
@@ -24,6 +34,7 @@ const ImageGallery = () => {
   const currentBucket = buckets.find(
     (busket) => busket.id === currentDataset?.bucket
   ); //@todo: update once we change arrays to object
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     if (datasets.length > 0) {
@@ -34,6 +45,7 @@ const ImageGallery = () => {
   if (datasets.length === 0) {
     return <Redirect to="/gallery" />;
   }
+
   return (
     <>
       <ImageGalleryHeader
@@ -58,16 +70,49 @@ const ImageGallery = () => {
             <Empty description="this dataset doesn't have any images yet, click to upload" />
           </Link>
         )}
+        <Grid item xs={12} sm={6} md={3}>
+          <TextField
+            className="upload-images__settings-item"
+            name="path"
+            label="Search By File Name"
+            value={searchTerm}
+            disabled={!datasetId}
+            onChange={(e: any) => {
+              setSearchTerm(e.target.value);
+            }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">/</InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment position="end">
+                  <SearchIcon />
+                </InputAdornment>
+              )
+            }}
+          />
+        </Grid>
 
         <Grid container spacing={2}>
-          {photos.map((tile: any) => (
-            <Grid item xs={6} sm={4} md={3} lg={2} key={tile.remote_path}>
-              <ImageCard
-                imageSrc={tile.remote_path}
-                xmlPath={tile.remote_xml_path}
-              />
-            </Grid>
-          ))}
+          {searchTerm
+            ? photos
+                .filter((tile: any) => tile.remote_path.includes(searchTerm))
+                .map((tile: any) => (
+                  <Grid item xs={6} sm={4} md={3} lg={2} key={tile.remote_path}>
+                    <ImageCard
+                      imageSrc={tile.remote_path}
+                      xmlPath={tile.remote_xml_path}
+                    />
+                  </Grid>
+                ))
+            : photos.map((tile: any) => (
+                <Grid item xs={6} sm={4} md={3} lg={2} key={tile.remote_path}>
+                  <ImageCard
+                    imageSrc={tile.remote_path}
+                    xmlPath={tile.remote_xml_path}
+                  />
+                </Grid>
+              ))}
         </Grid>
       </Container>
     </>
