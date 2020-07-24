dff2c5ac64454c5c11607053bc407996a0243e2e
Alvaro Castillo <Alvaro_Castillo@epam.com>
2020-07-24 13:44:30 -0500

    Alvaro: adding the filter to just archived task if are on saved status

--- frontend/src/components/tasksStatuses/DropdownButton.tsx
+++ frontend/src/components/tasksStatuses/DropdownButton.tsx
@@ -15,7 +15,8 @@ const menu: any = (
   );
   const isArchiveDisabled =
     !SelectedTasks.length ||
-    SelectedTasks.some((task) => task.status === 'archived');
+    SelectedTasks.some((task) => task.status === 'archived') ||
+    SelectedTasks.some((task) => task.status !== 'saved');
 
   const isRetryDisabled =
     !SelectedTasks.length ||
