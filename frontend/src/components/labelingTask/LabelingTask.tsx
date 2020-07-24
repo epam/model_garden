6e188e7fd7fe725d757cb343f24d8caa018b5d30
Svitlana Tsar <svitlana_tsar@epam.com>
2020-07-24 16:34:41 -0400

    Add ui reducer with loader

--- frontend/src/components/labelingTask/LabelingTask.tsx
+++ frontend/src/components/labelingTask/LabelingTask.tsx
@@ -1,20 +1,15 @@
 import React, { useState } from 'react';
 import { Task, FormData } from './task';
-import { ProgressLoader, SnackbarAlert } from '../shared';
+import { SnackbarAlert } from '../shared';
 import { connect, LabelingProps } from './util';
 
 const LabelingTaskComponent: React.FC<LabelingProps> = (props) => {
   const [error, setError] = useState('');
-  const [showLoader, setShowLoader] = useState(false);
 
   const handleTaskSubmit = (data: FormData) => {
-    props
-      .createLabelingTask(data)
-      .then(() => setShowLoader(false))
-      .catch(() => {
-        setShowLoader(false);
-        setError('Failed to create labeling task');
-      });
+    props.createLabelingTask(data).catch(() => {
+      setError('Failed to create labeling task');
+    });
   };
 
   return (
@@ -27,9 +22,7 @@ const LabelingTaskComponent: React.FC<LabelingProps> = (props) => {
         handleTaskSubmit={handleTaskSubmit}
         onDataSetChange={props.getUnsignedImagesCount}
         newTaskUrl={props.newTaskUrl}
-        setShowLoader={setShowLoader}
       />
-      <ProgressLoader show={showLoader} />
       <SnackbarAlert
         open={error !== ''}
         onClose={() => {
