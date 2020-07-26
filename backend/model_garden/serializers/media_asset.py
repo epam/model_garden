286ca1e0923f406273ba2ddeb3cb3c9f51040cc8
Rakshit_Desai <Rakshit_Desai@epam.com>
2020-07-26 14:51:07 -0700

    Rename fields in media_asset API

--- backend/model_garden/serializers/media_asset.py
+++ backend/model_garden/serializers/media_asset.py
@@ -5,18 +5,22 @@ from model_garden.models import MediaAsset
 
 
 class MediaAssetSerializer(serializers.ModelSerializer):
-  remote_xml_path = serializers.SerializerMethodField()
+  dataset_id = serializers.SerializerMethodField()
+  remote_label_path = serializers.SerializerMethodField()
 
   class Meta:
     model = MediaAsset
     fields = (
-      'dataset',
+      'dataset_id',
       'filename',
       'remote_path',
-      'remote_xml_path',
+      'remote_label_path',
     )
 
-  def get_remote_xml_path(self, obj: MediaAsset) -> str:
+  def get_dataset_id(self, obj: MediaAsset) -> str:
+    return obj.dataset.id
+
+  def get_remote_label_path(self, obj: MediaAsset) -> str:
     if obj.labeling_task and (obj.labeling_task.status == LabelingTaskStatus.SAVED
                               or obj.labeling_task.status == LabelingTaskStatus.ARCHIVED):
       return obj.remote_xml_path
