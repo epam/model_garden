ab66b0d1444427a3874f693a790f3f04ab3d3c5f
Svitlana Tsar <svitlana_tsar@epam.com>
2020-08-07 15:33:33 -0400

    Simplify request

--- frontend/src/api/gallery.api.ts
+++ frontend/src/api/gallery.api.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { backendHostPort } from './environment';
 
 export const getMediaAssetsRequest = ({ datasetId }: any) => {
-  const url = `http://${backendHostPort}/api/media-assets/${datasetId ? `?dataset_id=${datasetId}` : ''}`;
+  const url = `http://${backendHostPort}/api/media-assets/?dataset_id=${datasetId}`;
   return axios.get(url);
 };
