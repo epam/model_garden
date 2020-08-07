9e1b7a6163d4fd98e1f2d91d7b2fc2f66554fa09
Svitlana Tsar <svitlana_tsar@epam.com>
2020-08-07 15:18:45 -0400

    Use the only datasetId for the request. dataset_id is unique, so it is enough

--- frontend/src/api/gallery.api.ts
+++ frontend/src/api/gallery.api.ts
@@ -1,13 +1,7 @@
 import axios from 'axios';
 import { backendHostPort } from './environment';
 
-export const getMediaAssetsRequest = ({ bucketId, datasetId }: any) => {
-  const url = `http://${backendHostPort}/api/media-assets/${
-    bucketId
-      ? `?bucket_Id=${bucketId}${datasetId ? `&dataset_id=${datasetId}` : ''}`
-      : datasetId
-      ? `?dataset_id=${datasetId}`
-      : ''
-  }`;
+export const getMediaAssetsRequest = ({ datasetId }: any) => {
+  const url = `http://${backendHostPort}/api/media-assets/${datasetId ? `?dataset_id=${datasetId}` : ''}`;
   return axios.get(url);
 };
--- frontend/src/api/media.api.ts
+++ frontend/src/api/media.api.ts
@@ -39,14 +39,3 @@ export const addExistingDatasetRequest = async (bucketId: string, path: string) 
     }
   }
 };
-
-export const getImages = ({ bucketId, datasetId }: any) => {
-  const url = `http://${backendHostPort}/api/media-assets/${
-    bucketId
-      ? `?bucket_Id=${bucketId}${datasetId ? `&dataset_id=${datasetId}` : ''}`
-      : datasetId
-      ? `?dataset_id=${datasetId}`
-      : ''
-  }`;
-  return axios.get(url);
-};
