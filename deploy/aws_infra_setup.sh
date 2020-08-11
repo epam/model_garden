f26435e289ce0a1343c27594b2973bfa7f86a332
Illia Bielov <illia_bielov@epam.com>
2020-08-11 15:59:38 +0000

    Update aws_infra_setup.sh

--- deploy/aws_infra_setup.sh
+++ deploy/aws_infra_setup.sh
@@ -6,36 +6,39 @@
 #   * AWS listeners
 #
 # Usage:
-#   ENV_SUFFIX=prod ./infrastructure.sh
+#   ENV_SUFFIX= ./develop/aws_infra_setup.sh prod
 #
+
+#Get parameter for environment
+_ENV="${1:-devops}"
 
 # AWS load balancer presence check and creation
 
 # Load balancer Amazon Resource Number - LB ARN
-LBARN = 'aws elbv2 describe-load-balancers --names "model-garden-devops-lb" --query LoadBalancers[*].LoadBalancerArn --output text'
+LBARN = 'aws elbv2 describe-load-balancers --names "model-garden-${_ENV}-lb" --query LoadBalancers[*].LoadBalancerArn --output text'
 echo $LBARN
 
 if [$LBARN != ""]
 then
     echo "Load balancer is present"
 else
-    aws elbv2 create-load-balancer --name model-garden-devops-lb --subnets subnet-17d4107d subnet-e7dae99a subnet-dac9cf97 --security-groups sg-3b02fc5b sg-03b6a18f51a8cc356 sg-0153652719fde0da0 sg-0dbd4a9079111917d
+    aws elbv2 create-load-balancer --name model-garden-${_ENV}-lb --subnets subnet-17d4107d subnet-e7dae99a subnet-dac9cf97 --security-groups sg-3b02fc5b sg-03b6a18f51a8cc356 sg-0153652719fde0da0 sg-0dbd4a9079111917d
 fi
 
 # Update LB ARN after creation
-LBARN = 'aws elbv2 describe-load-balancers --names "model-garden-devops-lb" --query LoadBalancers[*].LoadBalancerArn --output text'
+LBARN = 'aws elbv2 describe-load-balancers --names "model-garden-${_ENV}-lb" --query LoadBalancers[*].LoadBalancerArn --output text'
 
 # AWS target group check and creation for frontend part
 # AWS listener creation for frontend
 
-tg_mg_frontend = 'aws elbv2 describe-target-groups --names model-garden-frontend-devops --query TargetGroups[*].TargetGroupArn --output text'
+tg_mg_frontend = 'aws elbv2 describe-target-groups --names model-garden-frontend-${_ENV} --query TargetGroups[*].TargetGroupArn --output text'
 echo $tg_mg_frontend
 
 if [$LBARN != "" -a $tg_mg_frontend != ""]
 then
     echo "load balancer frontend target group present"
 elif [$LBARN != ""]
-    aws elbv2 create-target-group --name model-garden-frontend-devops --protocol HTTP --port 80 --target-type ip --vpc-id vpc-88b8a5e3
+    aws elbv2 create-target-group --name model-garden-frontend-${_ENV} --protocol HTTP --port 80 --target-type ip --vpc-id vpc-88b8a5e3
     aws elbv2 create-listener --load-balancer-arn $lb_arn --protocol HTTP --port 80  --default-actions Type=forward,TargetGroupArn=$tg_mg_frontend
 else
     echo "Loadbalancer is not present"
@@ -44,13 +47,13 @@ fi
 # AWS target group check and creation for backend part
 # AWS listener creation for backend
 
-tg_mg_backend =  'aws elbv2 describe-target-groups --names model-garden-backend-devops --query TargetGroups[*].TargetGroupArn --output text'
+tg_mg_backend =  'aws elbv2 describe-target-groups --names model-garden-backend-${_ENV} --query TargetGroups[*].TargetGroupArn --output text'
 echo $tg_mg_backend
 if [$LBARN != "" -a $tg_mg_backend != ""]
 then
     echo "load balancer backend target group present"
 elif [$LBARN != ""]
-    aws elbv2 create-target-group --name model-garden-backend-devops --protocol HTTP --port 9000 --target-type ip --vpc-id vpc-88b8a5e3 --health-check-path /health_check/
+    aws elbv2 create-target-group --name model-garden-backend-${_ENV} --protocol HTTP --port 9000 --target-type ip --vpc-id vpc-88b8a5e3 --health-check-path /health_check/
     aws elbv2 create-listener --load-balancer-arn $lb_arn --protocol HTTP --port 9000  --default-actions Type=forward,TargetGroupArn=$tg_mg_backend
 else
     echo "Loadbalancer is not present"
