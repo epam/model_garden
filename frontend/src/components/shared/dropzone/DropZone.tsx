c89f4b5463c45b968a2c40c5459f4f5b3a55b160
Luis <luis_suarez@epam.com>
2020-08-14 16:22:37 -0500

    87 - update drozone to match mr comments

--- frontend/src/components/shared/dropzone/DropZone.tsx
+++ frontend/src/components/shared/dropzone/DropZone.tsx
@@ -1,73 +1,15 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { makeStyles } from '@material-ui/core';
 import { useDropzone } from 'react-dropzone';
 import zipSvg from '../../../assets/zip.svg';
-import { toast } from 'react-toastify';
-import blue from '@material-ui/core/colors/blue';
-import red from '@material-ui/core/colors/red';
-
-export interface DropZoneProps {
-  setFiles?: Function;
-  onDrop?: Function;
-}
-
-const useStyles = makeStyles((theme) => ({
-  dropField: {
-    flex: '1',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    padding: '1.5rem',
-    borderWidth: '2px',
-    borderRadius: '2px',
-    borderColor: blue[500],
-    borderStyle: 'dashed',
-    backgroundColor: blue[50],
-    color: blue[500],
-    outline: 'none',
-    transition: 'border 0.24s ease-in-out',
-    cursor: 'pointer'
-  },
-  dropFieldError: {
-    flex: '1',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    padding: '1.5rem',
-    borderWidth: '2px',
-    borderRadius: '2px',
-    borderColor: red[500], //only difference
-    borderStyle: 'dashed',
-    backgroundColor: blue[50],
-    color: blue[500],
-    outline: 'none',
-    transition: 'border 0.24s ease-in-out',
-    cursor: 'pointer'
-  },
-  dropText: {
-    width: '100%',
-    textAlign: 'center',
-    margin: '0'
-  },
-  dropTextError: {
-    width: '100%',
-    textAlign: 'center',
-    margin: '0',
-    color: 'red' //only difference
-  }
-}));
-
+import { blue, red } from '@material-ui/core/colors';
+import { ConformationDialog } from '../conformationDialog';
+import { isArchive, accept, DropZoneProps } from './utils';
 export const DropZone: React.FC<DropZoneProps> = ({
   setFiles,
   onDrop
 }: DropZoneProps) => {
-  const isArchive = (file: any) =>
-    file.type === 'application/zip' ||
-    file.type === 'application/x-zip-compressed';
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const {
     getRootProps,
@@ -76,31 +18,13 @@ export const DropZone: React.FC<DropZoneProps> = ({
     isDragAccept,
     isDragReject
   } = useDropzone({
-    onDrop: (acceptedFiles, fileRejections: any) => {
+    onDrop: (acceptedFiles, fileRejections) => {
       if (fileRejections.length) {
-        // option 1
-        toast.info(
-          `${
-            fileRejections.length > 1
-              ? `${fileRejections.length} files were ignored.
-              ${acceptedFiles.length} ${
-                  acceptedFiles.length === 1 ? 'was' : 'were'
-                } accepted.`
-              : `File "${fileRejections[0].file.name}" was ignored. Invalid format`
-          } `
-        );
-        //  //option2
-        //       toast.info(
-        //         `${acceptedFiles.length} files out of ${
-        //           acceptedFiles.length + fileRejections.length
-        //         } were accepted.Invalid format.`
-        //       );
+        setIsModalOpen(true);
       }
-      if (onDrop) {
-        onDrop(acceptedFiles);
-      }
-      if (setFiles) {
-        setFiles(
+      if (acceptedFiles.length) {
+        onDrop?.(acceptedFiles);
+        setFiles?.(
           acceptedFiles.map((file) =>
             Object.assign(file, {
               preview: isArchive(file) ? zipSvg : URL.createObjectURL(file)
@@ -109,34 +33,77 @@ export const DropZone: React.FC<DropZoneProps> = ({
         );
       }
     },
-    accept: `
-    image/bmp,
-    image/gif,
-    image/png,
-    image/jpeg,
-    image/svg,
-    image/tiff,
-    application/zip,
-    application/x-zip-compressed`
+    accept
   });
-  const classes = useStyles();
+
+  const classes = makeStyles((theme) => ({
+    dropField: {
+      flex: '1',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100%',
+      padding: '1.5rem',
+      borderWidth: '2px',
+      borderRadius: '2px',
+      borderColor: isDragReject ? red[500] : blue[500],
+      borderStyle: 'dashed',
+      backgroundColor: isDragReject ? red[50] : blue[50],
+      color: isDragReject ? red[500] : blue[500],
+      outline: 'none',
+      transition: 'border 0.24s ease-in-out',
+      cursor: 'pointer'
+    },
+    dropText: {
+      width: '100%',
+      textAlign: 'center',
+      margin: '0'
+    },
+    dropTextError: {
+      width: '100%',
+      textAlign: 'center',
+      margin: '0'
+    }
+  }))();
 
   return (
-    <section
-      {...getRootProps({
-        className: isDragReject ? classes.dropFieldError : classes.dropField
-      })}
-    >
-      <input {...getInputProps()} />
-      {isDragAccept && (
-        <p className={classes.dropText}>All files will be accepted</p>
-      )}
-      {isDragReject && (
-        <p className={classes.dropTextError}> Some files will be rejected</p>
-      )}
-      {!isDragActive && (
-        <p className={classes.dropText}>Drop some files here ...</p>
-      )}
-    </section>
+    <>
+      <section
+        {...getRootProps({
+          className: classes.dropField
+        })}
+      >
+        <input {...getInputProps()} />
+        {isDragAccept && (
+          <p className={classes.dropText}>All files will be accepted</p>
+        )}
+        {isDragReject && (
+          <p className={classes.dropText}>
+            Some files will be rejected because of wrong format.
+          </p>
+        )}
+        {!isDragActive && (
+          <p className={classes.dropText}>Drop some files here ...</p>
+        )}
+      </section>
+      <ConformationDialog
+        title="Some files were ignored. Wrong format"
+        open={isModalOpen}
+        setOpen={setIsModalOpen}
+        closeButton="Ok"
+      >
+        The only supported file types are
+        <ul>
+          <li> bmp</li>
+          <li>png</li>
+          <li>gif</li>
+          <li>jpeg</li>
+          <li>tiff</li>
+          <li>svg</li>
+          <li>zip</li>
+        </ul>
+      </ConformationDialog>
+    </>
   );
 };
