7054b59b68a5fe00637619f6c5688c50eee246a4
Svitlana Tsar <svitlana_tsar@epam.com>
2020-08-20 21:31:37 -0400

    Move Create Task form to the Dialog

--- frontend/src/routerconfig.tsx
+++ frontend/src/routerconfig.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { makeStyles, Tabs, Tab } from '@material-ui/core';
 import { Route, Switch, Link } from 'react-router-dom';
 
-import { AddDataset, LabelingTask, TasksStatuses, Gallery } from './components';
+import { AddDataset, TasksStatuses, Gallery } from './components';
 
 interface Config {
   component: FC;
@@ -25,19 +25,14 @@ const config: Config[] = [
     path: '/add-dataset'
   },
   {
-    component: LabelingTask,
-    label: 'Create Labeling Tasks',
-    path: '/labelling-task'
+    component: Gallery,
+    label: 'Gallery',
+    path: '/gallery'
   },
   {
     component: TasksStatuses,
     label: 'Tasks statuses',
     path: '/tasks-statuses'
-  },
-  {
-    component: Gallery,
-    label: 'Gallery',
-    path: '/gallery'
   }
 ];
 
