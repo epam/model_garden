bf15b1eacfb3cd6dd30aabab7fae0ce48bfdc0f0
Luis Suarez <luis_suarez@epam.com>
2020-08-22 02:56:40 +0000

    Resolve "Move 'Add Dataset' tab to the modal on the gallery page"

--- frontend/src/routerconfig.tsx
+++ frontend/src/routerconfig.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { makeStyles, Tabs, Tab } from '@material-ui/core';
 import { Route, Switch, Link } from 'react-router-dom';
 
-import { AddDataset, TasksStatuses, Gallery } from './components';
+import { TasksStatuses, Gallery } from './components';
 
 interface Config {
   component: FC;
@@ -19,11 +19,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const config: Config[] = [
-  {
-    component: AddDataset,
-    label: 'Add Dataset',
-    path: '/add-dataset'
-  },
   {
     component: Gallery,
     label: 'Gallery',
