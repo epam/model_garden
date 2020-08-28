c29cc3d215f0135c9c84ffe2bde131b78c5901f6
Svitlana Tsar <svitlana_tsar@epam.com>
2020-08-27 22:02:13 -0400

    Move styles from JSS to the SCSS

--- frontend/src/components/shared/dropzone/DropZone.tsx
+++ frontend/src/components/shared/dropzone/DropZone.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core';
 import { useDropzone } from 'react-dropzone';
 import zipSvg from '../../../assets/zip.svg';
-import { blue, red } from '@material-ui/core/colors';
 import { ConformationDialog } from '../conformationDialog';
 import { isArchive, accept, DropZoneProps } from './utils';
+import './styles.scss';
+
 export const DropZone: React.FC<DropZoneProps> = ({
   setFiles,
   onDrop
@@ -36,50 +36,24 @@ export const DropZone: React.FC<DropZoneProps> = ({
     accept
   });
 
-  const classes = makeStyles((theme) => ({
-    dropField: {
-      flex: '1',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100%',
-      padding: '1.5rem',
-      borderWidth: '2px',
-      borderRadius: '2px',
-      borderColor: isDragReject ? red[500] : blue[500],
-      borderStyle: 'dashed',
-      backgroundColor: isDragReject ? red[50] : blue[50],
-      color: isDragReject ? red[500] : blue[500],
-      outline: 'none',
-      transition: 'border 0.24s ease-in-out',
-      cursor: 'pointer'
-    },
-    dropText: {
-      width: '100%',
-      textAlign: 'center',
-      margin: '0'
-    }
-  }))();
-
   return (
     <>
       <section
         {...getRootProps({
-          className: classes.dropField
+          className: `mg-drop-field ${isDragReject ? 'rejected' : ''}`
         })}
       >
         <input {...getInputProps()} />
         {isDragAccept && (
-          <p className={classes.dropText}>All files will be accepted</p>
+          <p className="mg-drop-text">All files will be accepted</p>
         )}
         {isDragReject && (
-          <p className={classes.dropText}>
+          <p className="mg-drop-text">
             Some files will be rejected because of wrong format.
           </p>
         )}
         {!isDragActive && (
-          <p className={classes.dropText}>Drop some files here ...</p>
+          <p className="mg-drop-text">Drop some files here ...</p>
         )}
       </section>
       <ConformationDialog
--- frontend/src/components/shared/progressLoader/ProgressLoader.tsx
+++ frontend/src/components/shared/progressLoader/ProgressLoader.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import { Spin } from 'antd';
-import { makeStyles } from '@material-ui/core/styles';
 import { useTypedSelector } from '../../../store';
-
-const useStyles = makeStyles({
-  root: {
-    position: 'fixed',
-    zIndex: 1300,
-    right: '0',
-    bottom: '0',
-    top: '0',
-    left: '0',
-    padding: '2rem',
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+import './styles.scss';
 
 export const ProgressLoader: React.FC = () => {
   const showLoader = useTypedSelector(({ ui }) => ui.showLoader);
-  const classes = useStyles();
   if (!showLoader) return null;
   return (
-    <div className={classes.root}>
+    <div className="mg-spin-overlay">
       <Spin size="large" />
     </div>
   );
