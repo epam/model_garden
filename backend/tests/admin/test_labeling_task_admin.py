6c3ec0b5c50e845dbbcd53fc63563d86a16d06a5
Octavio G <octavio_garfio@epam.com>
2020-09-02 15:08:45 -0500

    171 add deleting of failed tasks in admin

--- backend/tests/admin/test_labeling_task_admin.py
+++ backend/tests/admin/test_labeling_task_admin.py
@@ -18,9 +18,38 @@ class TestLabelingTaskAdmin(BaseTestCase):
 
     self.assertListEqual(LabelingTaskAdmin.get_protected_by_status(tasks), [])
 
+  def test_get_protected_by_status_when_all_are_archived_and_failed(self):
+    tasks = [
+      LabelingTask(status=LabelingTaskStatus.FAILED),
+      LabelingTask(status=LabelingTaskStatus.ARCHIVED),
+    ]
+
+    self.assertListEqual(LabelingTaskAdmin.get_protected_by_status(tasks), [])
+
+  def test_get_protected_by_status_when_all_are_failed(self):
+    tasks = [
+      LabelingTask(status=LabelingTaskStatus.FAILED),
+      LabelingTask(status=LabelingTaskStatus.FAILED),
+    ]
+
+    self.assertListEqual(LabelingTaskAdmin.get_protected_by_status(tasks), [])
+
   def test_get_protected_by_status_when_some_tasks_are_not_archived(self):
     tasks = [
       LabelingTask(task_id=123, status=LabelingTaskStatus.ARCHIVED),
+      LabelingTask(task_id=456, status=LabelingTaskStatus.VALIDATION),
+      LabelingTask(task_id=789, status=LabelingTaskStatus.ANNOTATION),
+    ]
+
+    got = LabelingTaskAdmin.get_protected_by_status(tasks)
+
+    self.assertEqual(len(got), 2)
+    for index, task_id in enumerate(map(str, (456, 789))):
+      self.assertIn(task_id, got[index])
+
+  def test_get_protected_by_status_when_some_tasks_are_not_failed(self):
+    tasks = [
+      LabelingTask(task_id=123, status=LabelingTaskStatus.FAILED),
       LabelingTask(task_id=456, status=LabelingTaskStatus.VALIDATION),
       LabelingTask(task_id=789, status=LabelingTaskStatus.ANNOTATION),
     ]
