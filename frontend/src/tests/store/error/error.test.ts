20292460f1f202ed60b212457439118232bda996
Oleksii Druzhynin <oleksii_druzhynin@epam.com>
2020-09-03 19:48:57 +0000

    Adding redux-mock-store in to the app, and create the unit test infrastructure with a describe on every file and jest helper

---
+++ frontend/src/tests/store/error/error.test.ts
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom'; // Helper to use jest snippets
+
+describe('Tests for error store and reducers', () => {
+  // Tests goes here
+  it('this is a sample test', () => {});
+});
---
+++ frontend/src/tests/store/media/media.test.ts
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom'; // Helper to use jest snippets
+
+describe('Tests for media store and reducers', () => {
+  // Tests goes here
+  it('this is a sample test', () => {});
+});
