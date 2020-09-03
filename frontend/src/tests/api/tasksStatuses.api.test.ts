b59d47d4e4be103fb4a261efd366ad3903e6f328
Oleksii Druzhynin <oleksii_druzhynin@epam.com>
2020-09-03 19:48:57 +0000

    Adding redux-mock-store in to the app, and create the unit test infrastructure with a describe on every file and jest helper

---
+++ frontend/src/tests/api/tasksStatuses.api.test.ts
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom'; // Helper to use jest snippets
+
+describe('Test for TaskStatuses api', () => {
+  // Tests goes here
+  it('this is a sample test', () => {});
+});
---
+++ frontend/src/tests/components/shared/progressLoader/ProgressLoader.test.tsx
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom'; // Helper to use jest snippets
+
+describe('Tests for ProgressLoader component', () => {
+  // Tests goes here
+  it('this is a sample test', () => {});
+});
